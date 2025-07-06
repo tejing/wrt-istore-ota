51745a2f3059075411ce6a9b92a17a0bd1b34712
Jaykwok2999 <61024435@qq.com>
2025-07-06 12:05:52 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -143,6 +143,10 @@ sed -i 's/1.openwrt.pool.ntp.org/ntp2.aliyun.com/g' package/base-files/files/bin
 sed -i 's/2.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/time2.cloud.tencent.com/g' package/base-files/files/bin/config_generate
 
+# 更改时间戳
+rm -rf scripts/get_source_date_epoch.sh
+cp -af feeds/istoreos_ipk/patch/get_source_date_epoch.sh scripts/
+
 # 更改 banner
 rm -rf package/base-files/files/etc/banner
 cp -af feeds/istoreos_ipk/patch/istoreos-24.10/banner package/base-files/files/etc/
