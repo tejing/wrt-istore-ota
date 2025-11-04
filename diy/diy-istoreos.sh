bc7d2e87f2e2a2dc578af98acb0eb079d48ffb1b
Jaykwok2999 <61024435@qq.com>
2025-11-04 21:22:50 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -151,8 +151,8 @@ git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-passwall
 cp -af feeds/istoreos_ipk/patch/un.svg package/luci-app-passwall/root/www/luci-static/passwall/flags/
 
 # samba4
-rm -rf feeds/packages/libs/liburing
-git clone https://github.com/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
+#rm -rf feeds/packages/libs/liburing
+#git clone https://github.com/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
 rm -rf feeds/packages/net/samba4
 git clone https://github.com/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
 
